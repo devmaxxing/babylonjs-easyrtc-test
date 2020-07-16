b6023af5fd958a7b7da11cfe1b6250aec46846ee
Oliver Fei <ofei006@uottawa.ca>
2020-07-16 13:01:36 -0400

    Add room apis

--- server.js
+++ server.js
@@ -1,16 +1,40 @@
 // Load required modules
 const http    = require("http");              // http server core module
 const express = require("express");           // web framework external module
+const bodyParser = require("body-parser");
 const serveStatic = require('serve-static');  // serve static files
 const socketIo = require("socket.io");        // web socket external module
 const easyrtc = require("open-easyrtc"); // EasyRTC
+
+const rooms = new Set();
+let rtcApp;
 
 // Set process name
 process.title = "node-easyrtc";
 
 // Setup and configure Express http server. Expect a subfolder called "static" to be the web root.
 const app = express();
+
 app.use(serveStatic('static', {'index': ['index.html']}));
+
+app.use(bodyParser.json());
+app.post('/rooms', (req, res) => {
+    console.log(req.body);
+    rtcApp.createRoom(req.body.name, null, (err, obj) => {
+        if (err) {
+            console.err(err);
+        }
+        res.json(req.body);
+    });
+});
+
+app.get('/rooms', (req, res) => {
+    rtcApp.getRoomNames((err, roomNames) => {
+        res.json({
+            rooms: roomNames
+        });
+    });
+});
 
 // Start Express http server on port 8080
 const webServer = http.createServer(app);
@@ -27,12 +51,16 @@ easyrtc.events.on("roomJoin", function(connectionObj, roomName, roomParameter, c
 });
 
 // Start EasyRTC server
-const rtc = easyrtc.listen(app, socketServer, null, function(err, rtcRef) {
+rtc = easyrtc.listen(app, socketServer, null, function(err, rtcRef) {
     console.log("Initiated");
+    rtcRef.createApp("BabylonTest",null, function(err, appObj){
+        console.log("App Created!");
+        rtcApp = appObj;
+    });
 
     rtcRef.events.on("roomCreate", function(appObj, creatorConnectionObj, roomName, roomOptions, callback) {
         console.log("roomCreate fired! Trying to create: " + roomName);
-
+        rooms.add(roomName);
         appObj.events.defaultListeners.roomCreate(appObj, creatorConnectionObj, roomName, roomOptions, callback);
     });
 });

20a07a4b60c6e95a8f2315cce6a94381e62b61fe
Oliver Fei <ofei006@uottawa.ca>
2020-09-09 10:03:19 -0400

    fix turn config

--- server.js
+++ server.js
@@ -48,7 +48,9 @@ let myIceServers = [
     {"url":"stun:stun.l.google.com:19302"},
     {"url":"stun:stun1.l.google.com:19302"},
     {
-    "url":"turn:numb.viagenie.ca:3478"
+    "url":"turn:numb.viagenie.ca:3478",
+    "username":"[USERNAME]",
+    "credential":"[CREDENTIAL]"
     }
 ];
 
